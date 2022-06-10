53246a4571e04fa5bfdb2c49b21b49405aad9ce8
Jonathan Costa <jonathancosta746@gmail.com>
2022-06-10 10:42:57 -0300

    adicionado arquivos base

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './App.css'
+
+const App = () => {
+  const mensagem = "Olá Jonathan";
+
+  return <div className='container'>
+    
+    </div>;
+};
+
+export default App;
